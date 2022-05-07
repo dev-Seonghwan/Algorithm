8b6228bebcc20ba9a3fc3506462d6fd0ea53741b
milk-maca <norte0190@gmail.com>
2022-05-07 20:19:40 +0900

    Update 15596 - 정수 N개의 합.cpp

--- baekjoon/15596 - 정수 N개의 합.cpp
+++ baekjoon/15596 - 정수 N개의 합.cpp
@@ -1,26 +1,10 @@
 #include <vector>
-#include <iostream>
 using namespace std;
 
-long long sum(vector<int>& a)
+long long sum(vector<int> &a) 
 {
 	long long ans = 0;
-	for (vector<int>::iterator iter = a.begin(); iter < a.end(); iter++)
-	{	ans += *iter;
-		cout << &iter <<endl;
-
-	}
+    for(vector<int>::iterator iter = a.begin(); iter < a.end(); iter++)
+        ans += *iter;
 	return ans;
-}
-
-int main()
-{
-	vector<int> a;
-	a.push_back(1);
-	a.push_back(2);
-	a.push_back(3);
-	a.push_back(4);
-	a.push_back(5);
-
-	cout << sum(a);
 }

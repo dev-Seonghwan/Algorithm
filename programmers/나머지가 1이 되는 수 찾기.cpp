c2ca525f99db8891e10b5205d8da3f89f4dc2894
SeongHwan-Park <91909986+milk-maca@users.noreply.github.com>
2022-09-11 09:59:21 +0900

    Create 나머지가 1이 되는 수 찾기.cpp

---
+++ programmers/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) 
+{
+    for(int x = 2; x<n; x++)
+    {
+        if(n % x == 1)
+        {
+            return x;
+        }
+    }
+}

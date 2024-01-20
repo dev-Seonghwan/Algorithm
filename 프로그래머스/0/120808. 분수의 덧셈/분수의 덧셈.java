7f010f151c485749abe4a5b0e2ecd9bd9265c87f
SeongHwan Park <91909986+dev-Seonghwan@users.noreply.github.com>
2024-01-20 11:43:28 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.15 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        
+        
+        int n = numer1 * denom2 + numer2 * denom1;
+        int d = denom1 * denom2;
+        
+        for (int i = d; i > 1; i--){
+            if (n%i==0 &&d%i==0 ){
+                n/=i;
+                d/=i;
+            }
+            
+        }
+        
+        int[] answer = {n,d};
+        
+        
+        return answer;
+    }
+}

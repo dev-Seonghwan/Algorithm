71dbd0efe4cf6658e49aa395cee3e084e19a8510
SeongHwan Park <91909986+dev-Seonghwan@users.noreply.github.com>
2024-02-11 18:41:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = 0   
+    while answer <= 500:
+        if num == 1 :
+            return answer;
+        elif num % 2 == 0 :   
+            num = num/2
+        else :
+            num = num * 3 + 1            
+        answer += 1
+    return -1;
